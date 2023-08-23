393ee9c2205cc207fbd329047648795d684bc983
Luís Campanholi <lpcampanholi@gmail.com>
2023-08-22 22:48:18 -0300

    adicionando comentário

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ console.log('alteração realizado dentro do editor do github')
 console.log('mais uma alteração para testar o pull')
 console.log('mais uma alteração para testar o pull no terminal do VS CODE')
 console.log('adicionando mais um log')
+//um comentário
