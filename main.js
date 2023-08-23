bb2dd3ba7db368e4222dc29a78570e317a1e56ff
Luís Campanholi <lpcampanholi@gmail.com>
2023-08-22 22:02:38 -0300

    adicionando um console log

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-console.log('Olá mundo')
-console.log('segunda alteração')
-console.log('alteração realizado dentro do editor do github')
-console.log('mais uma alteração para testar o pull')
+console.log("Olá mundo");
+console.log("segunda alteração");
+console.log("alteração realizado dentro do editor do github");
+console.log("mais uma alteração para testar o pull");
+console.log("enviando para o GitHub");
