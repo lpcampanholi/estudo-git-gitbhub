3d58221488844578d09e0b55f90f63a346f1a174
Luís Campanholi <lpcampanholi@gmail.com>
2023-08-22 23:27:28 -0300

    voltando para trás umas versões aí

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 console.log('Olá mundo')
 console.log('segunda alteração')
 console.log('alteração realizado dentro do editor do github')
-console.log('mais uma alteração para testar o pull')
-console.log('mais uma alteração para testar o pull no terminal do VS CODE')
-console.log('adicionando mais um log')
-//um comentário
